61885fcd01b430260cb99ef62dfc699b9050bc77
Ω <JackGA6400@gmail.com>
2025-08-20 17:16:19 +0000

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+xargs rm -f < _delList.txt
+mkdir -p snd loc img beta/loc beta/img beta/snd
+wget -O grab.txt -U "Mozilla/5.0" "http://orteil.dashnet.org/patreon/grab.php"
+wget -O index.html -U "Mozilla/5.0" "https://orteil.dashnet.org/cookieclicker"
+xargs -I {} wget -P ./ "https://orteil.dashnet.org/cookieclicker/{}" < _miscList.txt
+sed -i "s|ajax('/patreon/grab.php'|ajax('grab.txt'|" main.js
+wget -qO- -U "Mozilla/5.0" https://orteil.dashnet.org/cookieclicker/snd/ | grep -oP '(?<=href=")[^"]+\.mp3' > snd/_sndList.txt
+wget -qO- -U "Mozilla/5.0" https://orteil.dashnet.org/cookieclicker/loc/ | grep -oP '(?<=href=")[^"]+\.js' > loc/_locList.txt
+wget -qO- -U "Mozilla/5.0" https://orteil.dashnet.org/cookieclicker/img/ | grep -oP '(?<=href=")[^"]+\.(png|jpe?g|db|gif)' > img/_imgList.txt
+xargs -a snd/_sndList.txt -I{} wget -O snd/{} "https://orteil.dashnet.org/cookieclicker/snd/{}"
+xargs -a loc/_locList.txt -I{} wget -O loc/{} "https://orteil.dashnet.org/cookieclicker/loc/{}"
+xargs -a img/_imgList.txt -I{} wget -O img/{} "https://orteil.dashnet.org/cookieclicker/img/{}"
+cd beta/
+wget -O index.html -U "Mozilla/5.0" "https://orteil.dashnet.org/cookieclicker/beta/"
+xargs -I {} wget -P ./ "https://orteil.dashnet.org/cookieclicker/beta/{}" < ../_miscList.txt
+sed -i "s|ajax('/patreon/grab.php'|ajax('../grab.txt'|" main.js
+wget -qO- -U "Mozilla/5.0" https://orteil.dashnet.org/cookieclicker/beta/snd/ | grep -oP '(?<=href=")[^"]+\.mp3' > snd/_sndList.txt
+wget -qO- -U "Mozilla/5.0" https://orteil.dashnet.org/cookieclicker/beta/loc/ | grep -oP '(?<=href=")[^"]+\.js' > loc/_locList.txt
+wget -qO- -U "Mozilla/5.0" https://orteil.dashnet.org/cookieclicker/beta/img/ | grep -oP '(?<=href=")[^"]+\.(png|jpe?g|db|gif)' > img/_imgList.txt
+xargs -a snd/_sndList.txt -I{} wget -O snd/{} "https://orteil.dashnet.org/cookieclicker/beta/snd/{}"
+xargs -a loc/_locList.txt -I{} wget -O loc/{} "https://orteil.dashnet.org/cookieclicker/beta/loc/{}"
+xargs -a img/_imgList.txt -I{} wget -O img/{} "https://orteil.dashnet.org/cookieclicker/beta/img/{}"
+cd ../
+find . -type f ! -path './.git/*' ! -name '_delList.txt' ! -name '_miscList.txt' ! -name 'README.md' ! -iname 'update.sh' > _delList.txt
+grep -Eho '["'\''][^/"'\'']+\.(js|css|html|json|xml)(\?[^"'\''"]*)?["'\'']' $(grep -Fxv '' _delList.txt) |
+grep -Ev '["'\''](https?:|\/\/)' |
+sed -E "s/^['\"]//;s/['\"].*//;s/\?.*//" |
+grep -v / | sort -u > _miscList.txt
